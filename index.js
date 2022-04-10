eafb4321ddbcd488e95525c012d2249b8c469702
Jonathan Koban <jon.koban@outlook.com>
2022-04-10 17:39:26 -0400

    commit tree 5bb551d5a3f6e306eaeeae2faeed96ccb89cae9c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+};
+
+function mondayWork(goto ='go to the office'){
+    return (`This Monday, I will ${goto}.`)
+}
+
+
+function wrapAdjective (wrap =' * '){
+    const inner = function(wrap2 = 'special'){
+        return(`You are ${wrap}${wrap2}${wrap}!`);
+    }
+    return inner
+}
